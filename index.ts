fb096c60a53062648313ab1c9038e6121cd4c17d
kamitsu <kamitsus@gmail.com>
2025-06-29 12:23:07 -0400

    update logging

--- index.ts
+++ index.ts
@@ -6,11 +6,28 @@ const client = new Cloudflare({
   apiToken: Bun.env.CLOUDFLARE_API_TOKEN!,
 });
 
+if (!Bun.env.CLOUDFLARE_EMAIL) {
+  throw new Error("CLOUDFLARE_EMAIL is not set");
+}
+if (!Bun.env.CLOUDFLARE_API_TOKEN) {
+  throw new Error("CLOUDFLARE_API_TOKEN is not set");
+}
+if (!Bun.env.CLOUDFLARE_ZONE_ID) {
+  throw new Error("CLOUDFLARE_ZONE_ID is not set");
+}
+
+function timeLog(...message: string[]) {
+  console.log(`[${new Date().toLocaleString()}] ${message.join(" ")}`);
+}
+
 async function updateIp() {
   try {
+    timeLog("checking public ip...");
     const publicIp = (await fetch("https://api.ipify.org?format=json").then(
       (res) => res.json(),
     )) as { ip: string };
+
+    timeLog("current public ip: ", publicIp.ip);
 
     for await (const recordResponse of client.dns.records.list({
       zone_id: Bun.env.CLOUDFLARE_ZONE_ID!,
@@ -21,19 +38,29 @@ async function updateIp() {
             zone_id: Bun.env.CLOUDFLARE_ZONE_ID!,
             content: publicIp.ip,
           });
+
+          timeLog("cloudflare dns updated!");
+        } else {
+          timeLog(
+            "no update needed. cloudflare dns record is the same as current ip address.",
+          );
         }
       }
     }
   } catch (error) {
-    console.error(error);
+    console.error(`[${new Date().toLocaleTimeString()}] ${error}`);
     throw error;
   }
 }
 
 const job = new CronJob(
-  "0 * * * * *",
+  "0 */5 * * * *",
   async () => {
-    await updateIp();
+    try {
+      await updateIp();
+    } catch (error) {
+      console.error(`[${new Date().toLocaleTimeString()}] ${error}`);
+    }
   },
   null,
   true,
